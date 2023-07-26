e174b14494133773a0a2c3349f89b40cfe183dd1
Mati Cordero <mcordero@arconsultores.com.ar>
2023-07-26 14:53:26 -0300

    Nose

--- src/main/java/com/example/primerApi/repository/IRMRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.primerApi.repository;
-
-import com.example.primerApi.model.Candidatos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRMRepository extends JpaRepository <Candidatos, Long>{
-}
-
-
